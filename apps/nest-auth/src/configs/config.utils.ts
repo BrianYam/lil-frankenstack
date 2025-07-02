ed4c077884d0e3b2ddd7af226877142f969b1d1a
brianyam <brianyam5931@gmail.com>
2025-07-02 16:24:43 +0800

    chore: add dynamic log level configuration based on environment variable

--- apps/nest-auth/src/configs/config.utils.ts
+++ apps/nest-auth/src/configs/config.utils.ts
@@ -1,4 +1,4 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import { InternalServerErrorException, LogLevel } from '@nestjs/common';
 
 export function getEnvVar(
   key: string,
@@ -49,4 +49,19 @@ export function getEnvVarAsBoolean(key: string, defaultValue = false): boolean {
   const value = process.env[key];
   if (!value) return defaultValue;
   return value.toLowerCase() === 'true';
+}
+
+// Define log levels based on environment variable
+export function getLogLevels(): LogLevel[] {
+  const logLevels: LogLevel[] = ['error', 'warn', 'log'];
+  if (
+    process.env.LOG_LEVEL === 'debug' ||
+    process.env.LOG_LEVEL === 'verbose'
+  ) {
+    logLevels.push('debug');
+    if (process.env.LOG_LEVEL === 'verbose') {
+      logLevels.push('verbose');
+    }
+  }
+  return logLevels;
 }
